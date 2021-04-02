312472c1bbd84960f8c358d8fae82e725787eb6e
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-02 10:37:49 -0500

    Add way to access cloudinary image url

--- app/serializers/exhibit_serializer.rb
+++ app/serializers/exhibit_serializer.rb
@@ -1,4 +1,14 @@
 class ExhibitSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :species_fact
-  has_one :zone
+  include Rails.application.routes.url_helpers
+  attributes :id, :name, :description, :species_fact, :exhibit_image, :zone_id
+
+  has_one :image
+  
+  def exhibit_image
+    if object.image.attached?
+      {
+        cloudinary: object.image.service_url
+      }
+    end
+  end
 end
